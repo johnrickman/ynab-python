a667178732c497b411957c439b34cf07c1325372
John Rickman <johnr@tandembyte.com>
2024-07-06 17:51:24 -0400

    Add request to get budget category by month

--- ynab/ynab/categories_api.py
+++ ynab/ynab/categories_api.py
@@ -230,3 +230,115 @@ class CategoriesApi(object):
             _preload_content=params.get('_preload_content', True),
             _request_timeout=params.get('_request_timeout'),
             collection_formats=collection_formats)
+
+    def get_category_by_id_month(self, budget_id, category_id, month, **kwargs):  # noqa: E501
+        """Single category for budget month  # noqa: E501
+
+        Returns a single category for specific month  # noqa: E501
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please pass use_async=True
+        >>> thread = api.get_category_by_id_month(budget_id, category_id, month, use_async=True)
+        >>> result = thread.get()
+
+        :param async bool
+        :param str budget_id: The ID of the Budget. (required)
+        :param str category_id: The ID of the Category. (required)
+        :param str month: Specific budget month. (required)
+        :return: CategoryResponse
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+        kwargs['_return_http_data_only'] = True
+        if kwargs.get('async'):
+            return self.get_category_by_id_month_with_http_info(budget_id, category_id, month, **kwargs)  # noqa: E501
+        else:
+            (data) = self.get_category_by_id_month_with_http_info(budget_id, category_id, month, **kwargs)  # noqa: E501
+            return data
+
+    def get_category_by_id_month_with_http_info(self, budget_id, category_id, month, **kwargs):  # noqa: E501
+        """Single category for budget month  # noqa: E501
+
+        Returns a single category  # noqa: E501
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please pass use_async=True
+        >>> thread = api.get_category_by_id_month_with_http_info(budget_id, category_id, month, use_async=True)
+        >>> result = thread.get()
+
+        :param async bool
+        :param str budget_id: The ID of the Budget. (required)
+        :param str category_id: The ID of the Category. (required)
+        :param str month: Specific budget month. (required)
+        :return: CategoryResponse
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+
+        all_params = ['budget_id', 'category_id', 'month']  # noqa: E501
+        all_params.append('async')
+        all_params.append('_return_http_data_only')
+        all_params.append('_preload_content')
+        all_params.append('_request_timeout')
+
+        params = locals()
+        for key, val in six.iteritems(params['kwargs']):
+            if key not in all_params:
+                raise TypeError(
+                    "Got an unexpected keyword argument '%s'"
+                    " to method get_category_by_id_month" % key
+                )
+            params[key] = val
+        del params['kwargs']
+        # verify the required parameter 'budget_id' is set
+        if ('budget_id' not in params or
+                params['budget_id'] is None):
+            raise ValueError("Missing the required parameter `budget_id` when calling `get_category_by_id_month`")  # noqa: E501
+        # verify the required parameter 'category_id' is set
+        if ('category_id' not in params or
+                params['category_id'] is None):
+            raise ValueError("Missing the required parameter `category_id` when calling `get_category_by_id_month`")  # noqa: E501
+        # verify the required parameter 'category_id' is set
+        if ('month' not in params or
+                params['month'] is None):
+            raise ValueError("Missing the required parameter `month` when calling `get_category_by_id_month`")  # noqa: E501
+
+        collection_formats = {}
+
+        path_params = {}
+        if 'budget_id' in params:
+            path_params['budget_id'] = params['budget_id']  # noqa: E501
+        if 'category_id' in params:
+            path_params['category_id'] = params['category_id']  # noqa: E501
+        if 'month' in params:
+            path_params['month'] = params['month']  # noqa: E501
+
+        query_params = []
+
+        header_params = {}
+
+        form_params = []
+        local_var_files = {}
+
+
+        body_params = None
+        # HTTP header `Accept`
+        header_params['Accept'] = self.api_client.select_header_accept(
+            ['application/json'])  # noqa: E501
+
+        # Authentication setting
+        auth_settings = ['bearer']  # noqa: E501
+
+        return self.api_client.call_api(
+            '/budgets/{budget_id}/months/{month}/categories/{category_id}', 'GET',
+            path_params,
+            query_params,
+            header_params,
+            body=body_params,
+            post_params=form_params,
+            files=local_var_files,
+            response_type='CategoryResponse',  # noqa: E501
+            auth_settings=auth_settings,
+            use_async=params.get('async'),
+            _return_http_data_only=params.get('_return_http_data_only'),
+            _preload_content=params.get('_preload_content', True),
+            _request_timeout=params.get('_request_timeout'),
+            collection_formats=collection_formats)
